71f8aa5316a951b43f82bd95eb04fcce2b2ed593
zhang kai <kai12_zhang@yahoo.co.jp>
2020-11-17 00:12:57 +0900

    test

---
+++ demo01/src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,33 @@
+package com.example.demo.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    Logger logger = LoggerFactory.getLogger(getClass());
+
+    @ResponseBody
+    @RequestMapping("/hello")
+    public String hello() {
+        return "Hello Word---zhangkai";
+    }
+
+    @RequestMapping("/success")
+    public String success(Model model) {
+        String name = "你好！！！";
+        model.addAttribute("name", name);
+        logger.trace("这是trace日志。。。");
+        logger.debug("这是debug日志。。。");
+        logger.info("这是info日志。。。");
+        logger.warn("这是warn日志。。。");
+        logger.error("这是error日志。。。");
+
+        return "success";
+    }
+}
